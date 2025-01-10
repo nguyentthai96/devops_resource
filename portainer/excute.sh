70b41083972bb0240f33156afe1c587ebf5d0310
Nguyễn Thành Thái <nguyentthai96@gmail.com>
2025-01-10 13:38:15 +0700

    feat[portainer] Add new Portainer Docker dashboard

---
+++ portainer/excute.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ./portainer
+if [ ! -f "./cert" ]; then
+  mkdir -p "./cert"
+#  https://gist.github.com/Kmaschta/205a67e42421e779edd3530a0efe5945
+  openssl req -x509 -newkey rsa:2048 -nodes -keyout ./cert/certificate.key -out ./cert/certificate.crt -days 365 \
+     -subj "/C=VN/ST=Vietnamese/L=Ho Chi Minh/O=NTTCo/OU=IT/CN=www.nttco.com"
+fi
+
+if ! docker secret inspect portainer.sslcert &> /dev/null; then
+  docker secret create portainer.sslcert ./cert/certificate.crt
+  docker secret create portainer.sslkey ./cert/certificate.key
+fi
+
+docker stack deploy -c portainer-agent-stack.yml portainer
+sleep 3
+# docker service inspect --format '{{ range .Spec.TaskTemplates }}{{ if eq .Mode.Name "replicated" }}{{ .ContainerSpec.Name }}{{ .ContainerSpec.DNSConfig.Domain }}{{ end }}{{ end }}' portainer
+#open https://localhost:9443
